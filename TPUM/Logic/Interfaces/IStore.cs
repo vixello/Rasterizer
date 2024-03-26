23fc815edadec88725c5513ddf3a688af788f0cd
Izabela K <aizeilla12344@gmail.com>
2024-03-26 14:27:10 +0100

    merge

---
+++ TPUM/Logic/Interfaces/IStore.cs
@@ -0,0 +1,16 @@
+﻿using Tpum.Data.Enums;
+
+namespace Tpum.Logic.Interfaces
+{
+    public interface IStore
+    {
+        public List<InstrumentDTO> GetAvailableInstruments();
+
+        public List<InstrumentDTO> GetInstrumentsByCategory(InstrumentCategory category);
+
+        public void DecrementInstrumentQuantity(Guid instrumentId);
+
+        public event EventHandler<ChangeProductPriceEventArgs> ProductPriceChange;
+        public event EventHandler<ChangeProductQuantityEventArgs> ProductQuantityChange;
+    }
+}
